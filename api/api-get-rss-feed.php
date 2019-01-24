4fc0ef02d53cab4f89f670b98fe5adb055f65b8a
Ben Greene <benrgreene@gmail.com>
2019-01-24 14:19:15 -0800

    feat: update endpoint output

--- api/api-get-rss-feed.php
+++ api/api-get-rss-feed.php
@@ -7,18 +7,16 @@ API_Register::get_instance()->add_endpoint(
 );
 
 function brg_api_get_rss_reader_content( $data ) {
-  $feed = get_xml_feed();
-  ob_start();
-  echo '<div class="feed">';
-  foreach( $feed as $item ) { ?>
-    <div class="item">
-      <h3 class="item__title"><?php echo $item['title']; ?></h3>
-      <p class="item__date"><?php echo $item['pubDate']; ?></p>
-      <p class="item__excerpt"><?php echo ((string) $item['description']); ?></p>
-      <a class="item__link" href="<?php echo $item['link']; ?>">View Full Article</a>
-    </div>
-  <?php }
-  echo '</div>';
-  $html_content = ob_get_clean();
-  return $html_content;
+  $feed = get_xml_feed(); ?>
+  <div class="feed">
+    <?php foreach( $feed as $item ) { ?>
+      <div class="item">
+        <h3 class="item__title"><?php echo $item['title']; ?></h3>
+        <p class="item__date"><?php echo date( 'M dS, Y', $item['pubDate'] ); ?></p>
+        <p class="item__excerpt"><?php echo ((string) $item['description']); ?></p>
+        <a class="item__link" href="<?php echo $item['link']; ?>">View Full Article</a>
+      </div>
+    <?php } ?>
+  </div>
+  <?php die();
 }
